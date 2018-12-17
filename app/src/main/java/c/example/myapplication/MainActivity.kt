235c6296e1fac08aac2cdbb836d36fd02c47636d
yamagami kenichi <kenichi-yamagami@altplus.co.jp>
2018-12-17 18:30:19 +0900

    mod ci branch

--- app/src/main/java/c/example/myapplication/MainActivity.kt
+++ app/src/main/java/c/example/myapplication/MainActivity.kt
@@ -10,6 +10,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        textView.text = "Morning Circle"
+        textView.text = "Evening Circle"
     }
 }
